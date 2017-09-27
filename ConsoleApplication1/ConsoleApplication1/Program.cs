dfba3cbdaece43f3ab14cb60558fbd6154edc78e
Alok Singh <alok.pro@mottosys.in>
2017-09-27 11:21:04 +0530

    Code Changes

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             //Code just added in GitHub
+            //Code just added in Visual Studio
         }
     }
 }
